ef0257248ea07fa0832c05dbe2e926c9b002b36f
Preethi Mourya <mopreethi03@gmail.com>
2026-09-22 17:54:50 +0530

    feat: initialize spring boot application

---
+++ src/main/java/com/preethi/banking/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.preethi.banking.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/api/health")
+    public String healthCheck() {
+        return "Digital Banking Platform V1 is running";
+    }
+}
